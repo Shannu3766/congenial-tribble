b752849a813ac634ff25ab62258c38e2844fd433
shannu3766 <shanmukhasrinivas82@gmail.com>
2025-01-29 13:32:44 +0530

    added new port

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def fetch_details():
 #     app.run(debug=True)
 #      # app.run(host='0.0.0.0', port=5000, debug=True)
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Get the PORT from environment variable
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # port = int(os.environ.get("PORT", 5000))  # Get the PORT from environment variable
+    app.run(host="0.0.0.0", port= 5000, debug=True)
