25ed89c9d6287d0b6f7f0503b0d60d492be56b0c
shannu3766 <shanmukhasrinivas82@gmail.com>
2025-01-29 13:21:12 +0530

    added new port

--- app.py
+++ app.py
@@ -51,6 +51,9 @@ def fetch_details():
     except Exception as e:
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+#      # app.run(host='0.0.0.0', port=5000, debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
-     # app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the PORT from environment variable
+    app.run(host="0.0.0.0", port=port, debug=True)
